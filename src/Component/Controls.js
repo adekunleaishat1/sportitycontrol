32bcf4939b922e23e84c633bedb782715341436b
adekunle aishat <aishatadekunle877@gmail.com>
2023-09-21 12:01:26 +0100

    my new commit

--- src/Component/Controls.js
+++ src/Component/Controls.js
@@ -1,74 +1,184 @@
-import React,{useEffect, useState,useRef} from 'react'
-import { FaPause, FaPhoneVolume, FaPlay, FaVolumeDown } from 'react-icons/fa'
-import { MdSkipNext, MdSkipPrevious} from 'react-icons/md'
-import Myicon from './Myicon';
+import React, { useEffect, useState, useRef } from "react";
+import {
+  FaPause,
+  FaPhoneVolume,
+  FaPlay,
+  FaShuttleVan,
+  FaVolumeDown,
+} from "react-icons/fa";
+import { BiShuffle } from "react-icons/bi";
+import { MdSkipNext, MdSkipPrevious } from "react-icons/md";
+import Myicon from "./Myicon";
+import { BsRepeat } from "react-icons/bs";
 
 const Controls = () => {
-  const [audioData, setAudioData] = useState('');
-  const [setdata, setsetdata] = useState(null)
+  const [setdata, setsetdata] = useState([]);
+  const [name, setname] = useState();
+  const [image, setimage] = useState();
   const [Playing, setPlaying] = useState(false);
+  const [CurrentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [volume, setVolume] = useState(99);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
   const audioRef = useRef(null);
 
-
-
   useEffect(() => {
-    fetch('http://localhost:2345/music', {
-      // method: 'GET',
-      // headers: {
-      //   'Content-Type': 'application/json',
-      // },
-    })
+    fetch("http://localhost:2345/music")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setsetdata(data)
-        const audioUrl = setdata[0].url;
-        console.log(audioUrl);
-        setAudioData(audioUrl);
-        audioRef.current.src = audioData
+        setsetdata(data);
+        console.log(setdata);
       })
       .catch((error) => {
-        console.error('Error fetching audio data:', error);
+        console.error("Error fetching audio data:", error);
       });
   }, []);
+  useEffect(() => {
+    if (setdata.length > 0) {
+      setimage(setdata[CurrentTrackIndex].image);
+      setname(setdata[CurrentTrackIndex].name);
+      const audioUrl = setdata[CurrentTrackIndex].audio;
+      audioRef.current.src = audioUrl;
+    }
+  }, [setdata]);
 
+  useEffect(() => {
+    const audio = audioRef.current;
+    audio.addEventListener("timeupdate", handleTimeUpdate);
+    audio.addEventListener("loadedmetadata", handleLoadedMetadata);
+    return () => {
+      audio.removeEventListener("timeupdate", handleTimeUpdate);
+      audio.removeEventListener("loadedmetadata", handleLoadedMetadata);
+    };
+  }, []);
 
-  const play = () =>{
-    if (audioRef.current.paused) {
-      audioRef.current.play();
+  const handleTimeUpdate = () => {
+    const audio = audioRef.current;
+    setCurrentTime(audio.currentTime);
+  };
+
+  const handleLoadedMetadata = () => {
+    const audio = audioRef.current;
+    setDuration(audio.duration);
+  };
+
+  const playmusic = () => {
+    const audio = audioRef.current;
+    if (audio.paused) {
+      audio.play();
       setPlaying(true);
     } else {
-      audioRef.current.pause();
+      audio.pause();
       setPlaying(false);
     }
-  }
+  };
+
+  const handleAudioEnded = () => {
+    setPlaying(false);
+  };
+
+  const handleNext = () => {
+    if (setdata.length > 0) {
+      setname(setdata[CurrentTrackIndex + 1].name);
+      setimage(setdata[CurrentTrackIndex + 1].image);
+      const audioUrl = setdata[CurrentTrackIndex + 1].audio;
+      audioRef.current.src = audioUrl;
+      setPlaying(false);
+      setCurrentTrackIndex((prevIndex) =>
+        prevIndex + 1 < setdata.length ? prevIndex + 1 : 0
+      );
+    } else {
+      alert("null");
+    }
+  };
+  const handlePrev = () => {
+    if (setdata.length > 0) {
+      const prevIndex =
+        CurrentTrackIndex - 1 < 0 ? setdata.length - 1 : CurrentTrackIndex - 1;
+      setname(setdata[prevIndex].name);
+      setimage(setdata[prevIndex].image);
+      const audioUrl = setdata[prevIndex].audio;
+      audioRef.current.src = audioUrl;
+      setPlaying(false);
+      setCurrentTrackIndex(prevIndex);
+    } else {
+      alert("null");
+    }
+  };
+  const handleVolumeChange = (event) => {
+    const newVolume = event.target.value;
+    setVolume(newVolume);
+    audioRef.current.volume = newVolume / 100;
+  };
+
+  const handleSeek = (event) => {
+    const newTime = (event.target.value / 100) * duration;
+    setCurrentTime(newTime);
+    audioRef.current.currentTime = newTime;
+  };
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    return formattedTime;
+  };
+
   return (
     <>
-      <div className='w-100 con'>
-        <audio ref={audioRef}   />
-        <div className='incon '>
-        <div className='w-50 mx-auto dd'>
-            <div className='d-flex justify-content-between align-items-center mx-auto dc'>
-                <button className='btn-lg fs-1 pr'><MdSkipPrevious/></button>
-                <button onClick={play} className='btn btn-dark pau'>{Playing ? <FaPause /> : <FaPlay />}</button>
-                <button className='btn-lg fs-1 pr'><MdSkipNext/></button>
-            </div>
-        </div>
-        <div className=' mx-auto ra'>
-          <div className=' w-100 d-flex justify-content-center align-items-center mx-auto ran'>
-              <span className=' sp '>0:00</span>
-              <input  type="range"  min="1" max="100" value="99" onchange="seekto()" />
-              <span className='sp'>0:00</span>
+      <div className="w-100 con">
+        <audio ref={audioRef} onEnded={handleAudioEnded}></audio>
+        <div className="audname">
+          <div className=" audnam">
+            <img className="audimg" src={image} alt="" />
+          </div>
+          <div className="shownam">
+            <p className="shorten-text">{name}</p>
           </div>
         </div>
+        <div className="incon ">
+          <div className="w-50 mx-auto dd">
+            <div className="d-flex justify-content-between align-items-center mx-auto dc">
+              <Myicon myclass="shu" icon={<BiShuffle />} />
+              <button onClick={handlePrev} className="btn-lg fs-1 pr">
+                <MdSkipPrevious />
+              </button>
+              <button onClick={playmusic} className="btn btn-dark pau">
+                {!Playing ? <FaPlay /> : <FaPause />}
+              </button>
+              <button onClick={handleNext} className="btn-lg fs-1 pr">
+                <MdSkipNext />
+              </button>
+              <Myicon myclass="shu" icon={<BsRepeat />} />
+            </div>
+          </div>
+          <div className=" mx-auto ra">
+            <div className=" w-100 d-flex justify-content-center align-items-center mx-auto ran">
+              <span className=" sp ">{formatTime(currentTime)}</span>
+              <input
+                type="range"
+                min="1"
+                max="100"
+                value={(currentTime / duration) * 100}
+                onChange={handleSeek}
+              />
+              <span className="sp">{formatTime(duration)}</span>
+            </div>
+          </div>
         </div>
-        <div className='volum'>
-          <Myicon myclass="volume" icon={<FaVolumeDown/>}/>
-         <input  type="range" min="1" max="100" value="99" onchange="setvolume()"/>
+        <div className="volum">
+          <Myicon myclass="volume" icon={<FaVolumeDown />} />
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={volume}
+            onChange={handleVolumeChange}
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Controls
+export default Controls;
--- src/Component/Sidenav.js
+++ src/Component/Sidenav.js
@@ -1,88 +1,158 @@
-import { useState, useRef } from 'react'
-import React  from 'react'
-import { FaArrowRight, FaPlus, FaSearch } from 'react-icons/fa'
-import {ImHome3} from 'react-icons/im'
-import {VscFolderLibrary} from 'react-icons/vsc'
-import Controls from './Controls'
-
+import { useState, useRef } from "react";
+import React from "react";
+import {
+  FaArrowCircleLeft,
+  FaArrowDown,
+  FaArrowRight,
+  FaBackward,
+  FaMusic,
+  FaPlus,
+  FaSearch,
+} from "react-icons/fa";
+import { ImHome3 } from "react-icons/im";
+import {MdArrowDropDown } from "react-icons/md";
+import { VscFolderLibrary } from "react-icons/vsc";
+import Controls from "./Controls";
+import Myicon from "./Myicon";
 
 const Sidenav = () => {
-   const [audioFile, setAudioFile] = useState(null);
-   const audioRef = useRef(null);
+  const [audioFile, setAudioFile] = useState(null);
+  const [imagefile, setimagefile] = useState(null)
+  const [imageData, setimageData] = useState(null)
+  const [audio, setAudioData] = useState(null);
+  const audioRef = useRef(null);
 
-   const handleFileChange = (event) => {
-      const file = event.target.files[0];
-      setAudioFile(file);
-      console.log(file);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setAudioFile(file);
+    console.log(file);
+  
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const audioData = event.target.result;
+      setAudioData(audioData);
     };
+    reader.readAsDataURL(file);
+  };
 
-    const Save = () => {
-      console.log(audioFile);
-      if (audioFile) {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          const audioData = event.target.result;
-         //  audioRef.current.src = audioData;
-         //   audioRef.current.play();
-           const name = audioFile.name;
-           const url = URL.createObjectURL(audioFile);
-             audioRef.current.src = url;
-             audioRef.current.pause();
+  const handleimageChange = (event) =>{
+    const myfile = event.target.files[0];
+    setimagefile(myfile);
+    console.log(myfile);
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const imageData = event.target.result;
+      setimageData(imageData);
+      console.log(imageData);
+    };
+    reader.readAsDataURL(myfile);
+  }
+  
 
-           fetch('http://localhost:2345/music', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name,  url }),
-      })
-        .then((response) => {
-          if (response.ok) {
-            console.log('Audio data successfully posted to the server.');
-          } else {
-            console.error('Error posting audio data to the server.');
-          }
+  const Save = () => {
+    console.log(audioFile);
+    console.log(imageData);
+    if (audioFile) {
+      const reader = new FileReader();
+  
+      reader.onload = (event) => {
+        const audioData = event.target.result;
+  
+        fetch("http://localhost:2345/music", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name: audioFile.name, audio: audioData, image: imageData }),
         })
-        .catch((error) => {
-          console.error('Error posting audio data:', error);
-        });
-        };
-        reader.readAsDataURL(audioFile);
-      }
-    };
+          .then((response) => {
+            if (response.ok) {
+              console.log("Audio data successfully posted to the server.");
+            } else {
+              console.error("Error posting audio data to the server.");
+            }
+          })
+          .catch((error) => {
+            console.error("Error posting audio data:", error);
+          });
+      };
+  
+      reader.readAsDataURL(audioFile);
+    }
+  };
+  
+  
   return (
     <>
-       <div className=' tt'>
-
-        <div className='ff'>
-          <div className='hm'>
-             <span className='im'><ImHome3/> </span>
-             <span className='li'>Home</span> 
+      <div className=" tt">
+        <div className="ff">
+          <div className="hm">
+            <span className="im">
+              <ImHome3 />{" "}
+            </span>
+            <span className="li">Home</span>
           </div>
-          <div className='hm'>
-             <span className='imi'><FaSearch/> </span>
-             <span className='lii'>Search</span>
+          <div className="hm">
+            <span className="imi">
+              <FaSearch />{" "}
+            </span>
+            <span className="lii">Search</span>
           </div>
         </div>
-        <div className='w-100 ft'>
-            <div className='d-flex justify-content-between align-items-center'>
-               <div className='hm'>
-                  <span className='imi'><VscFolderLibrary/> </span>
-                  <span className='lii'>Library</span>
-               </div>
-               <div className='d-flex justify-content-around align-items-center plu'>
-                <div><FaPlus/></div>
-                <div><FaArrowRight/></div>
-               </div>
+        <div className="w-100 ft">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="hm">
+              <span className="imi">
+                <VscFolderLibrary />{" "}
+              </span>
+              <span className="lii">Library</span>
             </div>
-            <input type="file" accept="audio/*" onChange={handleFileChange}  multiple/>
-            <button onClick={Save} >Save</button>
-            {/* <audio ref={audioRef} controls  ></audio> */}
-            {/* <Controls audiodata={audioData} />  */}
+            <div className="d-flex justify-content-around align-items-center plu">
+              <div>
+                <FaPlus />
+              </div>
+              <div>
+                <FaArrowRight />
+              </div>
+            </div>
+          </div>
+          <div className="d-flex justify-content-between align-items-center w-50 p-1 mt-2 ms-2">
+            <Myicon myclass="ico" icon={<FaPlus />} />
+            <button className="btn btn-light btn-sm pl">
+              playlist
+            </button>
+            <button className="btn btn-dark btn-sm pl">By you</button>
+          </div>
+          <div className="d-flex justify-content-between align-items-center arro">
+            <div className="col-5">
+              <Myicon myclass="lii" icon={<FaSearch />} /> 
+            </div>
+            <div className="col-3 arr2">
+              <p className="fw-bold ms-2 text-white rc">Recents</p>
+              <p className="ardown"><MdArrowDropDown /></p>
+            </div>
+          </div>
+          <div className="row mt-2 ps-2 ">
+            <div className="col-2">
+            <Myicon myclass="musci" icon={<FaMusic/>}/>
+            </div>
+            <div className="col-5 myplay">
+              <h1 className="fw-bold fs-6 text-white">My Playlists #1</h1>
+              <p className=" fs-6 gap-1">Aishat Adekunle</p>
+            </div>
+          </div>
+          <input
+            type="file"
+            accept="audio/*"
+            onChange={handleFileChange}
+            multiple
+          />
+          <input type="file" accept="image/*" onChange={handleimageChange} multiple/>
+          <button className="btn btn-light btn-sm" onClick={Save}>Save</button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Sidenav
+export default Sidenav;
